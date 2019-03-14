f1e9b1b5547f92f13e2016a028df8209df101387
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-14 11:45:46 +0300

    Add stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,14 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
+// The app itself is a widget.
+// Stateless widgets are immutable, meaning that their properties can't change—all values are final.
 class MyApp extends StatelessWidget {
 
   // A widget's main job is to provide a build method
   // that describes how to display the widget in terms of other, lower-level widgets
   @override
   Widget build(BuildContext context) {
-    // Create a pair of random words (using english_words package)
-    final wordPair = WordPair.random();
-
-    // The app itself is a widget
     return new MaterialApp(
       title: 'Welcome to Flutter',
       // The Scaffold widget, from the Material library, provides a default app bar,
@@ -24,10 +22,35 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         // The Center widget aligns its widget subtree to the center of the screen
-        body: Center( // these can't be constanst any more, because wordPair changes
-          child: Text(wordPair.asPascalCase),
+        body: Center(
+          child: RandomWords(),
         ),
       ),
     );
   }
 }
+
+// Stateful widgets maintain state that might change during the lifetime of the widget.
+// Implementing a stateful widget requires at least two classes:
+// 1) a StatefulWidget class that creates an instance of 2) a State class.
+// The StatefulWidget class is, itself, immutable,
+// but the State class persists over the lifetime of the widget.
+
+// This is the state of RandomWords widget
+// (notice that Dart can contain many classes in one file).
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    // Create a pair of random words (using english_words package)
+    final WordPair wordPair = WordPair.random();
+    // Notice that state builds widget
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+// This is stateful widget
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
