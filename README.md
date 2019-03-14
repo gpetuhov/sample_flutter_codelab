308c26a01d28335c245984a0a204d19e64bb1d7d
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-14 11:27:32 +0300

    Add external package

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Learn Flutter basics with Flutter Codelabs.
 * Flutter Plugin doesn't work on Android Studio Canary versions
 * If Android Studio can't find Dart SDK then provide path to it in Settings: path_to_flutter_folder/bin/cache/dart-sdk
 * To format text (if needed): Right-click the dart code and select Reformat Code with dartfmt
+* To add external packages: edit pubspec.yaml file and click Packages get and import package in main.dart
 
 ## References
 https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1/#0
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// Import external packages like this to use them
+import 'package:english_words/english_words.dart';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,6 +11,9 @@ class MyApp extends StatelessWidget {
   // that describes how to display the widget in terms of other, lower-level widgets
   @override
   Widget build(BuildContext context) {
+    // Create a pair of random words (using english_words package)
+    final wordPair = WordPair.random();
+
     // The app itself is a widget
     return new MaterialApp(
       title: 'Welcome to Flutter',
@@ -18,8 +24,8 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         // The Center widget aligns its widget subtree to the center of the screen
-        body: const Center(
-          child: const Text('Hello World'),
+        body: Center( // these can't be constanst any more, because wordPair changes
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
